1c22cb77780537452e2a60b09ad6dd50c99d702f
serhatbek <serhat.bekk@gmail.com>
2022-11-02 15:08:30 +0300

    header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.scss';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
+import SingleCocktail from './pages/SingleCocktail/SingleCocktail';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className='App'>
+      <Header />
       <Routes>
         <Route path='redux-cocktail-app/' element={<Home />} />
+        <Route
+          path='redux-cocktail-app/cocktail/:cocktailID'
+          element={<SingleCocktail />}
+        />
       </Routes>
     </div>
   );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.scss';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className='header'>
+      <div className='container'>
+        <Link className='header__brand' to={'redux-cocktail-app/'}>
+          <span>Find Your Own Cocktail</span>
+        </Link>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
